bc3777ae5309a8cbdf7d235900d052ba8e1d0378
256 <256>
2024-02-13 14:56:36 -0300

    <audit> tCHOKE

---
+++ contracts/TChoke.sol
@@ -0,0 +1,314 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+interface ITChokeHandler {
+    function handleDeposit(
+        address liquiditySource,
+        address from,
+        uint256 positionID
+    ) external returns (bytes32 depositID, uint256 tChokeAmount);
+
+    function handleWithdraw(
+        address liquiditySource,
+        address to,
+        uint256 positionID
+    ) external returns (bytes32 depositID);
+}
+
+contract TChoke is
+    Initializable,
+    ReentrancyGuardUpgradeable,
+    AccessControlUpgradeable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable
+{
+    bytes32 public constant LIQUIDITY_MANAGER_ROLE =
+        keccak256("LIQUIDITY_MANAGER");
+    bytes32 public constant DEBT_MANAGER_ROLE = keccak256("DEBT_MANAGER");
+
+    /**
+     * @dev Both representations of Arbitrum USDC (bridged or native) are valid in terms of liquidity availability.
+     */
+    address public constant USDCe = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address public constant USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+
+    uint256 public totalDebtCeiling;
+    uint256 public totalDebt;
+
+    struct LiquiditySourceHandler {
+        uint256 debtCeiling;
+        uint256 debt;
+        address handler;
+        bool paused;
+    }
+
+    /**
+     * @dev Mapping of valid liquidity sources. Apart from being listed, it also MUST have a valid `handler` contract and SHOULD NOT be paused.
+     *
+     * A `handler.debtCeiling` per liquidity source is implemented which its sum MUST add to `totalDebtCeiling` as well as `handler.debt` and `totalDebt`
+     */
+    mapping(address liquiditySource => LiquiditySourceHandler)
+        public liquiditySources;
+
+    mapping(bytes32 => uint256) public deposits;
+    mapping(bytes32 => address) public depositIDtoDepositor;
+
+    /**
+     * @dev Events
+     */
+
+    event Deposit(
+        address indexed liquiditySource,
+        address indexed depositor,
+        uint256 indexed positionID,
+        uint256 amount
+    );
+
+    event Withdraw(
+        address indexed liquiditySource,
+        address indexed depositor,
+        uint256 indexed positionID,
+        uint256 amount
+    );
+
+    event AddLiquiditySource(
+        address indexed liquiditySource,
+        address indexed handler
+    );
+
+    event RemoveLiquiditySource(address indexed liquiditySource);
+
+    event SetDebtCeiling(
+        address indexed liquiditySource,
+        uint256 debtCeiling,
+        bool paused
+    );
+
+    /**
+     * @dev CustomErrors
+     */
+
+    error TChokeInvalidLiquidityPosition(address liquiditySource);
+    error TChokeInvalidDeposit(bytes32);
+    error TChokeInvalidHandler();
+    error TChokeDebtCeilingExceeded();
+    error TChokePaused();
+
+    function initialize(uint256 initialSupply) public initializer {
+        __ReentrancyGuard_init();
+        __AccessControl_init();
+        __ERC20_init("tChoke", "tChoke");
+        __ERC20Burnable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(LIQUIDITY_MANAGER_ROLE, msg.sender);
+        _grantRole(DEBT_MANAGER_ROLE, msg.sender);
+
+        _mint(msg.sender, initialSupply);
+    }
+
+    /**
+     * @dev To be executed by any user with `LIQUIDITY_MANAGER` permission.
+     * Previous handler must have been removed if existing, which in turn must have the handlers' debt fully paid.
+     * @param liquiditySource Any address representing underlying liquidity. How liquidity is managed or liquidated is up to handler. Debt ceiling must be set per liquidity source.
+     * @param handler MUST implement `handleDeposit` and `handleWithdraw` in its interface. Is up to the handler contract to return the proper amount of tChoke corresponding to each liquidity source and how `depositID` is calculated.
+     *
+     * Emits a {AddLiquiditySource} event.
+     *
+     */
+
+    function addLiquiditySource(
+        address liquiditySource,
+        address handler
+    ) external onlyRole(LIQUIDITY_MANAGER_ROLE) {
+        if (
+            handler == address(0) ||
+            liquiditySources[liquiditySource].handler != address(0) ||
+            liquiditySources[liquiditySource].debt != 0
+        ) revert TChokeInvalidHandler();
+
+        liquiditySources[liquiditySource] = LiquiditySourceHandler(
+            0,
+            0,
+            handler,
+            false
+        );
+
+        emit AddLiquiditySource(liquiditySource, handler);
+    }
+
+    /**
+     * @dev To be executed by any user with `LIQUIDITY_MANAGER` permission.
+     * Removing any liquiditySource must be consistent with `totalDebt` and `totalDebtCeiling`
+     * In order to ensure liquidity parameters invariant, `handler.debt` and `handler.debtCeiling` MUST be zero.
+     * @param liquiditySource Any address representing underlying liquidity previously added using {addLiquiditySource}.
+     *
+     * Emits a {RemoveLiquiditySource} event.
+     *
+     */
+
+    function removeLiquiditySource(
+        address liquiditySource
+    ) external onlyRole(LIQUIDITY_MANAGER_ROLE) {
+        if (
+            liquiditySources[liquiditySource].handler == address(0) ||
+            liquiditySources[liquiditySource].debt != 0 ||
+            liquiditySources[liquiditySource].debtCeiling != 0
+        ) revert TChokeInvalidHandler();
+
+        liquiditySources[liquiditySource] = LiquiditySourceHandler(
+            0,
+            0,
+            address(0),
+            false
+        );
+
+        emit RemoveLiquiditySource(liquiditySource);
+    }
+
+    /**
+     * @dev To be executed by any user with `DEBT_MANAGER` permission.
+     * Single entrypoint for modifying totalDebtCeiling.
+     * In order to ensure liquidity parameters invariant, `handler.debt` and `handler.debtCeiling` MUST be zero.
+     * @param liquiditySource Any address representing underlying liquidity previously added using {addLiquiditySource}.
+     * @param _debtCeiling New `debtCeiling` for specified `liquiditySource`. Previous `handler.debtCeiling` will be substracted from `totalDebtCeiling` first.
+     * @param _paused Will pause liquiditySource and disable depositing for that specific source.
+     *
+     * Emits a {SetDebtCeiling} event.
+     */
+
+    function setDebtCeiling(
+        address liquiditySource,
+        uint256 _debtCeiling,
+        bool _paused
+    ) external onlyRole(DEBT_MANAGER_ROLE) {
+        if (liquiditySources[liquiditySource].handler == address(0))
+            revert TChokeInvalidHandler();
+
+        totalDebtCeiling =
+            totalDebtCeiling -
+            liquiditySources[liquiditySource].debtCeiling;
+
+        liquiditySources[liquiditySource].debtCeiling = _debtCeiling;
+
+        totalDebtCeiling = totalDebtCeiling + _debtCeiling;
+
+        liquiditySources[liquiditySource].paused = _paused;
+
+        emit SetDebtCeiling(liquiditySource, _debtCeiling, _paused);
+    }
+
+    /**
+     * @dev Generic method for depositing various liquidity sources and minting the corresponding amount of tChoke.
+     * Considering any active debt, the handler and total debt cannot exceed either `handler.debtCeiling` or `totalDebtCeiling`.
+     *
+     * Each deposit must have a unique bytes32 `depositID` which is stored along the debt as receipt of the corresponding liquidity.
+     *
+     * Emits a {Deposit} event.
+     */
+
+    function deposit(
+        address liquiditySource,
+        uint256 positionID
+    ) external nonReentrant {
+        if (totalDebt > totalDebtCeiling) revert TChokeDebtCeilingExceeded();
+
+        LiquiditySourceHandler storage handler = liquiditySources[
+            liquiditySource
+        ];
+
+        if (
+            liquiditySource == address(0) ||
+            handler.handler == address(0) ||
+            positionID == 0 ||
+            handler.debt > handler.debtCeiling
+        ) revert TChokeInvalidLiquidityPosition(liquiditySource);
+
+        if (handler.paused) revert TChokePaused();
+
+        ITChokeHandler tChokeHandler = ITChokeHandler(handler.handler);
+
+        (bytes32 depositID, uint256 tChokeAmount) = tChokeHandler.handleDeposit(
+            liquiditySource,
+            _msgSender(),
+            positionID
+        );
+
+        if (
+            tChokeAmount == 0 ||
+            deposits[depositID] != 0 ||
+            depositIDtoDepositor[depositID] != address(0)
+        ) revert TChokeInvalidLiquidityPosition(liquiditySource);
+
+        if (
+            (totalDebt + tChokeAmount) > totalDebtCeiling ||
+            (handler.debt + tChokeAmount) > handler.debtCeiling
+        ) revert TChokeDebtCeilingExceeded();
+
+        deposits[depositID] = tChokeAmount;
+
+        totalDebt = totalDebt + tChokeAmount;
+        handler.debt = handler.debt + tChokeAmount;
+        depositIDtoDepositor[depositID] = _msgSender();
+
+        _mint(_msgSender(), tChokeAmount);
+
+        emit Deposit(liquiditySource, _msgSender(), positionID, tChokeAmount);
+    }
+
+    /**
+     * @dev Method for withdrawing any liquidity representation from the `handler` vault, which must ensure that the liquidity is in fact transferred. It relies on the _burn mechanism of {ERC20} which decreases _totalSupply and reverts in case _msgSender() does not have enough balance.
+     * It also expects to find _msgSender() -depositor- associated with `depositID` for redundancy.
+     *
+     * Emits a {Deposit} event.
+     *
+     * NOTE: Debts are per account-basis. The actual debt does not depend nor varies with the usd value of the underlying. It is only calculated when deposited and not recalculated during `withdraw`.
+     *
+     */
+
+    function withdraw(
+        address liquiditySource,
+        uint256 positionID
+    ) external nonReentrant {
+        LiquiditySourceHandler storage handler = liquiditySources[
+            liquiditySource
+        ];
+
+        if (handler.handler == address(0) || positionID == 0)
+            revert TChokeInvalidLiquidityPosition(liquiditySource);
+
+        ITChokeHandler tChokeHandler = ITChokeHandler(handler.handler);
+
+        bytes32 depositID = tChokeHandler.handleWithdraw(
+            liquiditySource,
+            _msgSender(),
+            positionID
+        );
+
+        uint256 tChokeAmount = deposits[depositID];
+        if (
+            tChokeAmount == 0 || depositIDtoDepositor[depositID] != _msgSender()
+        ) revert TChokeInvalidDeposit(depositID);
+
+        _burn(_msgSender(), tChokeAmount);
+
+        deposits[depositID] = 0;
+
+        totalDebt = totalDebt - tChokeAmount;
+        handler.debt = handler.debt - tChokeAmount;
+        depositIDtoDepositor[depositID] = address(0);
+
+        emit Withdraw(liquiditySource, _msgSender(), positionID, tChokeAmount);
+    }
+
+    function isUSDC(address token) external pure returns (bool) {
+        return token == USDCe || token == USDC;
+    }
+}
---
+++ contracts/TChokeCamelotV2Handler.sol
@@ -0,0 +1,359 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+interface IOracle {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+interface ITChoke {
+    function deposits(bytes32 depositID) external pure returns (uint256);
+
+    function depositIDtoDepositor(
+        bytes32 depositID
+    ) external pure returns (address);
+
+    function isUSDC(address token) external pure returns (bool);
+
+    function decimals() external pure returns (uint256);
+}
+
+interface ICamelotPair {
+    function token0() external pure returns (address);
+
+    function token1() external pure returns (address);
+
+    function totalSupply() external pure returns (uint256);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 _reserve0,
+            uint112 _reserve1,
+            uint16 _token0FeePercent,
+            uint16 _token1FeePercent
+        );
+}
+
+interface ILiquiditySource {
+    function transferFrom(address from, address to, uint256 tokenId) external;
+
+    function ownerOf(uint256 tokenId) external view returns (address);
+
+    function getPoolInfo()
+        external
+        view
+        returns (
+            address lpToken,
+            address grailToken,
+            address xGrailToken,
+            uint256 lastRewardTime,
+            uint256 accRewardsPerShare,
+            uint256 lpSupply,
+            uint256 lpSupplyWithMultiplier,
+            uint256 allocPoint
+        );
+
+    function getStakingPosition(
+        uint256 tokenId
+    )
+        external
+        view
+        returns (
+            uint256 amount,
+            uint256 amountWithMultiplier,
+            uint256 startLockTime,
+            uint256 lockDuration,
+            uint256 lockMultiplier,
+            uint256 rewardDebt,
+            uint256 boostPoints,
+            uint256 totalMultiplier
+        );
+}
+
+/**
+ * @dev TChokeCamelotV2Handler.
+ * Contract to handle any liquidity source compatible with spNFT infrastructure of Camelot V2 DEX.
+ *
+ * NOTE Because of the _receipts system, each instance should only be used to handle one liquiditySource -e.g. one liquidity pool-
+ */
+
+contract TChokeCamelotV2Handler is Context {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    ITChoke immutable TChoke;
+    address immutable oracle;
+
+    uint8 constant USDC_DECIMALS = 6;
+
+    // Mapping from holder address to their (enumerable) set of owned tokens
+    mapping(address => EnumerableSet.UintSet) private _receipts;
+    mapping(uint256 => address) public tokenIdToDepositor;
+
+    error TChokeInvalidDeposit(bytes32 depositID);
+    error TChokeInvalidPositionTransfer();
+    error TChokeInvalidSender();
+    error TChokeInvalidLiquidityPool();
+    error TChokeHandlerFailedOracle();
+
+    /**
+     * @dev Both main functions `handleDeposit` and `handleWithdraw` should only be callable from TChoke itself.
+     */
+    modifier onlyTChoke() {
+        if (_msgSender() != address(TChoke)) revert TChokeInvalidSender();
+        _;
+    }
+
+    constructor(address _tChokeAddress, address _oracle) {
+        require(_tChokeAddress != address(0), "Invalid TChoke");
+        require(_oracle != address(0), "Invalid oracle");
+
+        TChoke = ITChoke(_tChokeAddress);
+        oracle = _oracle;
+    }
+
+    /**
+     * @dev Proxy to do the actual `deposit` of the liquidity on behalf of tChoke. Only callable by `TChoke` itself.
+     *
+     * @param liquiditySource Must have been listed as a valid liquiditySource in TChoke. For this specific handler, `liquiditySource` is expected to comply with spNFT interface.
+     * @param from Actual caller of the function forwarded to this contract to assign receipts and balances
+     * @param positionID The `tokenID` of the `liquiditySource` -spNFT-
+     *
+     * @return depositID {bytes32} Identifier of the deposit
+     * @return tChokeAmount {uint256} Representation of the USD value of the underlying with 18 decimals.
+     *
+     * NOTE: Depending on the actual liquidity conditions, an external oracle may be used to validate a certain threshold when calculating `tChokeAmount` from the actual USD underlying value.
+     */
+
+    function handleDeposit(
+        address liquiditySource,
+        address from,
+        uint256 positionID
+    ) external onlyTChoke returns (bytes32, uint256) {
+        bytes32 depositID = _calculateDepositID(
+            liquiditySource,
+            from,
+            positionID
+        );
+
+        if (
+            tokenIdToDepositor[positionID] != address(0) ||
+            TChoke.depositIDtoDepositor(depositID) != address(0) ||
+            TChoke.deposits(depositID) != 0
+        ) revert TChokeInvalidDeposit(depositID);
+
+        ILiquiditySource source = ILiquiditySource(liquiditySource);
+
+        source.transferFrom(from, address(this), positionID);
+
+        _receipts[from].add(positionID);
+        tokenIdToDepositor[positionID] = from;
+
+        if (source.ownerOf(positionID) != address(this))
+            revert TChokeInvalidPositionTransfer();
+
+        (uint256 usdProvided, , ) = _calculateUSDProvided(source, positionID);
+        if (usdProvided == 0) revert TChokeInvalidDeposit(depositID);
+
+        return (depositID, usdProvided);
+    }
+
+    /**
+     * @dev Proxy to do the actual `withdraw` of the liquidity on behalf of tChoke. Only callable by `TChoke` itself.
+     *
+     * @param liquiditySource Must have been listed as a valid liquiditySource in TChoke. For this specific handler, `liquiditySource` is expected to comply with spNFT interface.
+     * @param to Actual caller of the function forwarded to this contract to assign receipts and balances
+     * @param positionID The `tokenID` of the `liquiditySource` -spNFT-
+     *
+     * @return depositID {bytes32} Identifier of the deposit which suffice getting the actual debt of the deposit. Debt is not recalculated.
+     *
+     */
+
+    function handleWithdraw(
+        address liquiditySource,
+        address to,
+        uint256 positionID
+    ) external onlyTChoke returns (bytes32) {
+        bytes32 depositID = _calculateDepositID(
+            liquiditySource,
+            to,
+            positionID
+        );
+
+        if (
+            TChoke.deposits(depositID) == 0 ||
+            tokenIdToDepositor[positionID] != to ||
+            TChoke.depositIDtoDepositor(depositID) != to
+        ) revert TChokeInvalidDeposit(depositID);
+
+        ILiquiditySource source = ILiquiditySource(liquiditySource);
+
+        source.transferFrom(address(this), to, positionID);
+
+        _receipts[to].remove(positionID);
+        tokenIdToDepositor[positionID] = address(0);
+
+        if (source.ownerOf(positionID) != to)
+            revert TChokeInvalidPositionTransfer();
+
+        return (depositID);
+    }
+
+    function receiptsBalance(address _owner) external view returns (uint256) {
+        require(
+            _owner != address(0),
+            "TChoke: balance query for the zero address"
+        );
+        return _receipts[_owner].length();
+    }
+
+    function receiptOfOwnerByIndex(
+        address _owner,
+        uint256 _index
+    ) external view returns (uint256) {
+        require(
+            _owner != address(0),
+            "TChoke: balance query for the zero address"
+        );
+        return _receipts[_owner].at(_index);
+    }
+
+    function getDepositInformation(
+        address liquiditySource,
+        uint256 positionID
+    ) external view returns (uint256, uint256, uint256, uint256) {
+        ILiquiditySource source = ILiquiditySource(liquiditySource);
+
+        (
+            uint256 usdProvided,
+            uint256 lpAmount,
+            uint256 totalSupply
+        ) = _calculateUSDProvided(source, positionID);
+
+        return (positionID, usdProvided, lpAmount, totalSupply);
+    }
+
+    function getDepositInformation(
+        address liquiditySource,
+        address depositor,
+        uint256 positionID
+    ) external view returns (uint256, uint256, uint256, uint256, uint256) {
+        ILiquiditySource source = ILiquiditySource(liquiditySource);
+
+        (
+            uint256 usdProvided,
+            uint256 lpAmount,
+            uint256 totalSupply
+        ) = _calculateUSDProvided(source, positionID);
+
+        bytes32 depositID = _calculateDepositID(
+            liquiditySource,
+            depositor,
+            positionID
+        );
+
+        return (
+            positionID,
+            usdProvided,
+            lpAmount,
+            totalSupply,
+            TChoke.deposits(depositID)
+        );
+    }
+
+    function _calculateUSDProvided(
+        ILiquiditySource source,
+        uint256 positionID
+    ) private view returns (uint256, uint256, uint256) {
+        (address lpTokenAddress, , , , , , , ) = source.getPoolInfo();
+        (uint256 lpAmount, , , , , , , ) = source.getStakingPosition(
+            positionID
+        );
+
+        ICamelotPair camelotPair = ICamelotPair(lpTokenAddress);
+        (uint112 _reserve0, uint112 _reserve1, , ) = camelotPair.getReserves();
+
+        uint256 totalSupply = camelotPair.totalSupply();
+
+        uint112 usdcReserve;
+        uint256 usdProvided;
+
+        (, int256 oraclePrice, , , ) = IOracle(oracle).latestRoundData();
+        if (oraclePrice == 0) revert TChokeHandlerFailedOracle();
+
+        if (TChoke.isUSDC(camelotPair.token0())) {
+            usdcReserve = _reserve0;
+            _validateOraclePrice(_reserve1, usdcReserve, oraclePrice);
+        } else if (TChoke.isUSDC(camelotPair.token1())) {
+            usdcReserve = _reserve1;
+            _validateOraclePrice(_reserve0, usdcReserve, oraclePrice);
+        } else {
+            revert TChokeInvalidLiquidityPool();
+        }
+
+        usdProvided =
+            ((usdcReserve * lpAmount) / totalSupply) *
+            (10 ** (TChoke.decimals() - USDC_DECIMALS));
+
+        return (usdProvided, lpAmount, totalSupply);
+    }
+
+    function _validateOraclePrice(
+        uint256 assetReserves,
+        uint256 usdcReserves,
+        int256 latestAnswer
+    ) private pure {
+        // Assuming any asset with 18 decimals and usdcReserves always with 6 decimals
+        // Ratio will result in the usdcPrice with 8 decimals
+        int256 ratio = int256((usdcReserves * (10 ** 20)) / assetReserves);
+        // 2% threshold
+        int256 threshold = (latestAnswer * 2) / 100;
+        if (ratio > latestAnswer) {
+            if (ratio - latestAnswer > threshold) {
+                revert TChokeHandlerFailedOracle();
+            }
+        } else {
+            if (latestAnswer - ratio > threshold) {
+                revert TChokeHandlerFailedOracle();
+            }
+        }
+    }
+
+    /**
+     * @dev Calculates a unique {bytes32} keccak256 encoding:
+     *  - The address of this smart contract (representing each instance of TChokeCamelotV2Handler)
+     *  - The actual liquidity source represented by the spNFT wrapper of any valid Camelot V2 liquidity pool
+     *  - The address owner of the position, which will have to repay for the debt in order to withdraw the spNFT.
+     *  - The position -token- ID. This, along the liquidity source address, ensures uniqueness.
+     *
+     * @return depositID {bytes32}
+     */
+    function _calculateDepositID(
+        address liquiditySource,
+        address owner,
+        uint256 positionID
+    ) private view returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    address(this),
+                    liquiditySource,
+                    owner,
+                    positionID
+                )
+            );
+    }
+}
